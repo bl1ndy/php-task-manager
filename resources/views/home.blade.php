d584aa8328f3f403a5470d495dc45adb4c795d50
bl1ndy <ayoblindstone@gmail.com>
2021-06-16 14:45:20 +0300

    edit 'home' view according to demonstrating project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,11 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <div class="jumbotron">
+        <h1 class="display-4">Привет от Хекслета!</h1>
+        <p class="lead">Практические курсы по программированию</p>
+        <hr class="my-4">
+        <a class="btn btn-primary btn-lg" href="https://hexlet.io" role="button">Узнать больше</a>
     </div>
 </div>
 @endsection
