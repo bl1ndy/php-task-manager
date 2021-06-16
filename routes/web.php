131e2851c2cac0cc4e709f9d010cb9fe0de075d2
blindy <ayoblindstone@gmail.com>
2021-06-16 22:28:22 +0300

    add resource routing for task statuses

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\TaskStatusController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +21,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('task_statuses', TaskStatusController::class);
