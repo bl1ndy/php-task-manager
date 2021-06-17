eae600735e71eaa2ed8a595a2c7d5e43659c91a6
blindy <ayoblindstone@gmail.com>
2021-06-17 23:59:07 +0300

    add authorization gates for task statuses actions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        //
     ];
 
     /**
@@ -25,6 +26,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('create', function () {
+            return Auth::user();
+        });
+
+        Gate::define('update', function () {
+            return Auth::user();
+        });
+
+        Gate::define('destroy', function () {
+            return Auth::user();
+        });
     }
 }
