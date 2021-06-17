ddaa68c7169c9bfe6a5bf603655ca8e24f58d60d
bl1ndy <ayoblindstone@gmail.com>
2021-06-17 17:12:23 +0300

    add 'create' form

--- resources/views/task_status/create.blade.php
+++ resources/views/task_status/create.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    Hello!
-</div>
+
+<h1 class="mb-5">Создать статус</h1>
+{{ Form::model($taskStatus, ['url' => route('task_statuses.store'), 'class' => 'w-50']) }}
+    <div class="form-group">
+        {{ Form::label('name', 'Имя') }}
+        {{ Form::text('name', null, ['class' => $errors->any() ? 'form-control is-invalid' : 'form-control']) }}
+        @error('name')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+        @enderror
+    </div>
+    {{ Form::submit('Создать', ['class' => 'btn btn-primary']) }}
+{{ Form::close() }}
+
 @endsection
