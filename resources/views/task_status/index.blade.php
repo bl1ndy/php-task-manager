50979969f4d0560a5a0458c05892e9b4a6398989
bl1ndy <ayoblindstone@gmail.com>
2021-06-17 17:12:04 +0300

    add 'create' button, table, pagination

--- resources/views/task_status/index.blade.php
+++ resources/views/task_status/index.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
 <div class="container">
-    Hello!
+    <h1 class="mb-5">Статусы</h1>
+
+    <a href="{{ route('task_statuses.create') }}" class="btn btn-primary">Создать статус</a>
+
+    <table class="table mt-2">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Имя</th>
+                <th>Дата создания</th>
+                <th>Действия</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($taskStatuses as $status)
+            <tr>
+                <td>{{ $status->id }}</td>
+                <td>{{ $status->name }}</td>
+                <td>{{ $status->created_at }}</td>
+                <td>
+                    <a class="text-danger" href="" data-confirm="Вы уверены?" data-method="delete">Удалить</a>
+                    <a href="">Изменить</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{ $taskStatuses->links() }}
 </div>
 @endsection
