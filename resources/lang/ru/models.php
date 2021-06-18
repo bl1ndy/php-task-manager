4741e713c3e254fa188ead2d98a09b59d5a41b74
bl1ndy <ayoblindstone@gmail.com>
2021-06-18 15:59:48 +0300

    add i18n localization

---
+++ resources/lang/ru/models.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'task_status' => [
+        'id' => 'Id',
+        'name' => 'Имя',
+        'created_at' => 'Дата создания'
+    ]
+];
---
+++ resources/lang/ru/views.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'task_status' => [
+        'index' => [
+            'statuses' => 'Статусы',
+            'actions' => 'Действия',
+            'buttons' => [
+                'create' => 'Создать статус',
+                'edit' => 'Изменить',
+                'delete' => 'Удалить'
+            ]
+        ],
+        'create' => [
+            'create_status' => 'Создать статус',
+            'buttons' => [
+                'create' => 'Создать',
+            ]
+        ],
+        'edit' => [
+            'edit_status' => 'Изменение статуса',
+            'buttons' => [
+                'update' => 'Обновить',
+            ]
+        ]
+    ]
+];
