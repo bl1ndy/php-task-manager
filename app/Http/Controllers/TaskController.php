e95fe2a3c4c55a6a25821ec5024665e6916cb30a
bl1ndy <ayoblindstone@gmail.com>
2021-06-21 16:16:40 +0300

    add task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use App\Models\TaskStatus;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::paginate();
+        return view('task.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        Gate::authorize('task_create');
+
+        $task = new Task();
+        $taskStatuses = TaskStatus::all()
+            ->mapWithKeys(fn($status) => [$status->id => $status->name])
+            ->all();
+        $executors = User::all()
+            ->mapWithKeys(fn($user) => [$user->id => $user->name])
+            ->all();
+
+        return view('task.create', compact('task', 'taskStatuses', 'executors'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'name' => 'required|unique:tasks',
+            'status_id' => 'required',
+            'assigned_to_id' => ''
+        ]);
+
+        $task = new Task();
+        $task->fill($data);
+        $task->created_by_id = Auth::user()->id;
+        $task->save();
+
+        flash(__('messages.task.create.success'))->success();
+
+        return redirect()
+            ->route('tasks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
