101fc8ebe5970de15520c26ba2b5afebd9b4bf99
bl1ndy <ayoblindstone@gmail.com>
2021-06-23 16:46:40 +0300

    add label controller

---
+++ app/Http/Controllers/LabelController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLabelRequest;
+use App\Models\Label;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class LabelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $labels = Label::paginate();
+
+        return view('labels.index', compact('labels'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        Gate::authorize('label_actions');
+
+        $label = new label();
+
+        return view('labels.create', compact('label'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLabelRequest $request)
+    {
+        $data = $request->validated();
+
+        $label = new Label();
+        $label->fill($data);
+        $label->save();
+
+        flash(__('messages.label.create.success'))->success();
+
+        return redirect()
+            ->route('labels.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Label $label)
+    {
+        Gate::authorize('label_actions');
+
+        return view('label.edit', compact('label'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
